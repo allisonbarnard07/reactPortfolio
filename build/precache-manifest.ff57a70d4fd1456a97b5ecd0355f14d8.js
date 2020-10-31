94b428081f05a8f0f492adbff959ac1f941bd415
Allison Barnard <allisonbarnard07@gmail.com>
2020-10-31 10:14:27 -0400

    updating

---
+++ build/precache-manifest.ff57a70d4fd1456a97b5ecd0355f14d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b475544e3ec69c204c2d199462101a",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "321bd1913052221618a2",
+    "url": "/reactPortfolio/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "adf15966f6d1fb24d557",
+    "url": "/reactPortfolio/static/css/main.70c4fedc.chunk.css"
+  },
+  {
+    "revision": "321bd1913052221618a2",
+    "url": "/reactPortfolio/static/js/2.47a65b80.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/reactPortfolio/static/js/2.47a65b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf15966f6d1fb24d557",
+    "url": "/reactPortfolio/static/js/main.a79026cb.chunk.js"
+  },
+  {
+    "revision": "24736e6795fa0c1201b3",
+    "url": "/reactPortfolio/static/js/runtime-main.21fff2c0.js"
+  }
+]);
